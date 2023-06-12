ea2761dfba73e20b399c702d0ef82028814cb347
Taj Sapra <sapra.taj@live.com>
2023-06-13 02:53:03 +0530

    To create a header like navbar using bootstrap/Tailwind for all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route,
+  Link
+} from "react-router-dom";
+import React, {useState} from 'react'
+import Header from './Components/Unit Components/Header';
+import Home from './Components/Pages/Home';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
   );
 }
 
