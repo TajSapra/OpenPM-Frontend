2cd3eeda65a10482e724f685e45cf6fa0556af7c
Taj Sapra <sapra.taj@live.com>
2023-06-14 23:20:11 +0530

    Home page done. Building About us.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import {
   BrowserRouter as Router,
   Routes, 
   Route,
-  Link
+  Link,
+  useNavigate
 } from "react-router-dom";
 import React, {useState} from 'react'
 import Header from './Components/Unit Components/Header';
 import Home from './Components/Pages/Home';
+import About from './Components/Pages/About';
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <Header title="OpenPM"/>
+      <div id='main_content' style={{height:'86.5vh', overflow: 'scroll'}}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+
+      </div>
     </>
   );
 }
