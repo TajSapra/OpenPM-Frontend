2cb06649ee15ef2349d3115a117b911d0a04732e
Taj Sapra <sapra.taj@live.com>
2023-06-17 00:43:42 +0530

    Login Bugs Completed.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Link,
   useNavigate
 } from "react-router-dom";
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from './Components/Unit Components/Header';
 import Home from './Components/Pages/Home';
 import Login from './Components/Pages/Login'
@@ -14,9 +14,42 @@ import About from './Components/Pages/About';
 import Signup from './Components/Pages/Signup'
 import Profile from './Components/Unit Components/Profile';
 import Logout from './Components/Pages/Logout';
+import { getCookie, setCookie } from '../src/Components/Unit Components/CookieUtils';
+
 function App() {
   const [user, SetUser]=useState({})
+  const navigate=useNavigate()
   if(user==undefined)SetUser({})
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      const secretCookie = getCookie('secret');
+      const emailCookie = getCookie('email');
+
+      const verified = await fetch('http://localhost:8000/app/get_user', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ emailid: emailCookie, secret: secretCookie }),
+      });
+      const responsedata = await verified.json();
+
+      if (responsedata['success'] == 'Updated Successfully') {
+        SetUser(responsedata.user)
+      }
+      else{
+        SetUser({});
+        setCookie('secret', '', { expires: new Date(0) });
+        setCookie('email', '', { expires: new Date(0) });
+        if(window.location.href=='http://localhost:3000/login'||window.location.href=='http://localhost:3000/signup'){
+
+        }
+        else{
+          navigate('/');
+        }
+      }
+    };
+
+    checkLoggedIn();
+  }, [SetUser, navigate]);  
   return (
     <>
       <Header title="OpenPM" user={user} setuser={SetUser}/>
@@ -24,8 +57,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home user={user} setuser={SetUser}/>}></Route>
           <Route path='/about' element={<About user={user} setuser={SetUser}/>}></Route>
-          <Route path="/login" element={<Login user={user} setuser={SetUser}/>}></Route>
-          <Route path="/signup" element={<Signup user={user} setuser={SetUser}/>}></Route>
+          <Route path="/login" element={<Login user={user} setuser={SetUser} path="/login"/>}></Route>
+          <Route path="/signup" element={<Signup user={user} setuser={SetUser} path="/signup"/>}></Route>
           <Route path='/app/profile' element={<Profile user={user} setuser={SetUser}/>}></Route>
         </Routes>
       </div>
